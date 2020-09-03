70ea7fceb45b8be1320db7769243311c784552ca
mizumochi1080 <70096105+mizumochi1080@users.noreply.github.com>
2020-09-03 16:35:56 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(function () {
-    $("#minus").click(function () {
+    $(".minus").click(function () {
         let cnt = Number($("#cnt").text());
         if (cnt == 0) {
             $("#cnt").text(99);
@@ -7,7 +7,7 @@ $(function () {
             $("#cnt").text(cnt - 1);
         }
     });
-    $("#plus").click(function () {
+    $(".plus").click(function () {
         let cnt = Number($("#cnt").text());
         $("#cnt").text((cnt + 1) % 100);
     });
